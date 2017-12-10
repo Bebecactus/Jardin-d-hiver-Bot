960e6a0c8bd79c45c6e2b461b1db165ccc0c88fd
Bebecactus <34408111+Bebecactus@users.noreply.github.com>
2017-12-10 04:23:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('I am ready!');
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-  if (message.content === 'You are super') {
-    message.reply('You are a hero');
-  }
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-client.login('Mzg4NDYwNTg2MTY3Njk3NDE5.DQtYZA.EgMgeSd10w84Tg2_2mSVrVE0aJE');
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
